dbe45e14368f3f94bcd0bfeeeebb645ad254768c
WiktorKuch <wiktor.kucharek1997@gmail.com>
2022-12-04 15:28:05 +0100

    Serializacja Prosta gra

---
+++ SerializacjaProstaGra/SerializacjaProstaGra/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializacjaProstaGra
+{
+    public class Player
+    {
+
+#pragma warning disable CS8618 // Pole niedopuszczające wartości null musi zawierać wartość inną niż null podczas kończenia działania konstruktora. Rozważ zadeklarowanie pola jako dopuszczającego wartość null.
+        public string Name { get; set; }
+#pragma warning restore CS8618 // Pole niedopuszczające wartości null musi zawierać wartość inną niż null podczas kończenia działania konstruktora. Rozważ zadeklarowanie pola jako dopuszczającego wartość null.
+        public int Level { get; set; }
+        public int HealthPoints { get; set; }
+#pragma warning disable CS8618 // Pole niedopuszczające wartości null musi zawierać wartość inną niż null podczas kończenia działania konstruktora. Rozważ zadeklarowanie pola jako dopuszczającego wartość null.
+        public List<Statistic> Statistics { get; set; }
+#pragma warning restore CS8618 // Pole niedopuszczające wartości null musi zawierać wartość inną niż null podczas kończenia działania konstruktora. Rozważ zadeklarowanie pola jako dopuszczającego wartość null.
+
+    }
+}
---
+++ SerializacjaProstaGra/SerializacjaProstaGra/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Reflection.Emit;
+using System.Xml.Linq;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace SerializacjaProstaGra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var player = new Player() { 
+            
+                Name = "Mario",
+                Level = 1,
+                HealthPoints = 100,
+                Statistics = new List<Statistic>()
+                { 
+                    new Statistic()
+                    {
+                        Name = "Strength",
+                        Points = 10
+                    },
+                    new Statistic()
+                    {
+                        Name = "Inteligence",
+                        Points = 10
+                    }
+
+                }
+
+            };
+
+            player.Level++;
+
+                                        //  Serializacja 
+
+            var playerSerialized=JsonConvert.SerializeObject(player); //serializuje wartość do string
+
+            File.WriteAllText(@"C:\Users\Wiktro Kucharek\append\playerSerialized.json", playerSerialized);//zapisuje z-serializowaną wartość do pliku
+            
+
+                                        // Deserializacja
+
+            string deserializedPlayer = File.ReadAllText(@"C:\Users\Wiktro Kucharek\append\playerSerialized.json");  //deserializowana wartość z pliku przypisana do string
+            Player deserPlayer = JsonConvert.DeserializeObject<Player>(deserializedPlayer); //z-deserializowana wartość na obiekt player 
+
+            Console.WriteLine(deserPlayer.Name); // wyświetla imie gracza z pliku 
+
+        }
+    }
+}
